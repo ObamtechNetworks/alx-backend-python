2b8a04137995582cdcd18b68a488650ed8105466
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-01 21:02:47 +0100

    docstring

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,6 +5,15 @@ from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """A function that defines ann array or list of zoom levels
+
+    Args:
+        lst (Tuple): A tuple of zoom items
+        factor (int, optional): An integer factor. Defaults to 2.
+
+    Returns:
+        List: Returns an array of zoom levels
+    """
     zoomed_in: Tuple = tuple([
         item for item in lst
         for i in range(factor)
