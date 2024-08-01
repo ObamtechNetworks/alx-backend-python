5419470706345f3f9abeb394ccf598584f832962
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-01 16:18:59 +0100

    first annotation

---
+++ 0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Basic Python Annotations
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Basic annotations
+    Add takes two floats: a, b
+    and returns their sum as float
+    """
+
+    return a + b
