9c9f136aaa4ebb08065922e5bb7eee580d62b212
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-01 17:36:57 +0100

    basic annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic annotations continued"""
+
+
+def floor(n: float) -> int:
+    """Function that takes a float and
+    returns the floor (int) of the float
+
+    Args:
+        n (float): The float parameter
+
+    Returns:
+        int: Returns an integer
+    """
+    return float.__floor__(n)
