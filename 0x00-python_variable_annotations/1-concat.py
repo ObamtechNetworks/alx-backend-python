881c802d5faa2989a345fc432dd2eb8988d5da05
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-01 17:27:59 +0100

    more string annoatations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Basic annotations
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that concatenates two string
+    and returns the concatenated string
+
+    Args:
+        str1 (str): first string
+        str2 (str): second string
+
+    Returns:
+        str: concatenation of str1 + str2
+    """
+    return str1 + str2
