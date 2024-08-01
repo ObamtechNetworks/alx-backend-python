143baad9f2413391ea20ad39e592652a197be349
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-01 17:47:08 +0100

    float to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """A function that takes a float n
+    and returns the string representation of the float
+
+    Args:
+        n (float): the float object
+
+    Returns: String reprenstation of the float variable
+    """
+    return str(n)
